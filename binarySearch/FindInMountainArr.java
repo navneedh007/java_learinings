3a51d5dc90daf2d9aa40845275f2bf0c9b938f41
navneedh007 <navneedhkrishna@gmail.com>
2024-03-21 14:45:40 +0530

    Java

--- binarySearch/FindInMountainArr.java
+++ binarySearch/FindInMountainArr.java
@@ -1,22 +1,72 @@
 //1095 Leet code, Find in Mountain Array
 public class FindInMountainArr {
     public static void main(String[] args) {
-        int[] arr={-1,0,1,2,1,0};
-        int target = 1;
+        int[] arr = {1, 2, 3, 4, 5, 3, 1};
+        int target = 3;
+        int ans = new FindInMountainArr().search(arr, target);
+        System.out.println(ans);
     }
-    static int peakIndex1(int[] arr){
-        int start=0;
-        int end=arr.length-1;
-        while(start<end){
-            int mid=start+(end-start)/2;    
-            if(arr[mid+1]<arr[mid]){
-                end=mid;
-            }
-            else {
-                start=mid+1;
-            }  
+    int search(int[] arr, int target) {
+        int peak = peakIndexInMountainArray(arr);
+        int firstTry = orderAgnosticBS(arr, target, 0, peak);
+        if (firstTry != -1) {
+            return firstTry;
         }
-        return start;
-      
-    }   
+        // try to search in second half
+        return orderAgnosticBS(arr, target, peak+1, arr.length - 1);
+    }
+
+    public int peakIndexInMountainArray(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] > arr[mid+1]) {
+                // you are in dec part of array
+                // this may be the ans, but look at left
+                // this is why end != mid - 1
+                end = mid;
+            } else {
+                // you are in asc part of array
+                start = mid + 1; // because we know that mid+1 element > mid element
+            }
+        }
+        // in the end, start == end and pointing to the largest number because of the 2 checks above
+        // start and end are always trying to find max element in the above 2 checks
+        // hence, when they are pointing to just one element, that is the max one because that is what the checks say
+        // more elaboration: at every point of time for start and end, they have the best possible answer till that time
+        // and if we are saying that only one item is remaining, hence cuz of above line that is the best possible ans
+        return start; // or return end as both are =
+    }
+
+    static int orderAgnosticBS(int[] arr, int target, int start, int end) {
+        // find whether the array is sorted in ascending or descending
+        boolean isAsc = arr[start] < arr[end];
+
+        while(start <= end) {
+            // find the middle element
+//            int mid = (start + end) / 2; // might be possible that (start + end) exceeds the range of int in java
+            int mid = start + (end - start) / 2;
+
+            if (arr[mid] == target) {
+                return mid;
+            }
+
+            if (isAsc) {
+                if (target < arr[mid]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            } else {
+                if (target > arr[mid]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            }
+        }
+        return -1;
+    }  
 }
--- binarySearch/FirstAndLastPosition.java
+++ binarySearch/FirstAndLastPosition.java
@@ -1,8 +1,11 @@
 //leet code problem no.34
+//Given an array of integers nums sorted in non-decreasing order, find the starting and ending position of a given target value.
+//If target is not found in the array, return [-1, -1].
+//  You must write an algorithm with O(log n) runtime complexity.
 public class FirstAndLastPosition {
     public static void main(String[] args) {
 
-        int[] arr = {5, 7, 7, 8, 8, 8};
+        int[] arr = {5, 7, 7, 8, 8, 8, 9, 10,11 ,12, 13, 14};
         int target = 8;
         int[] ans = new FirstAndLastPosition().searchRange(arr, target);
         System.out.println(ans[0] + " " + ans[1]);
